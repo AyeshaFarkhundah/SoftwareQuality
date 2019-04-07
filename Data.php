c24c1277a99ec60cffe2d50f8ee10276d61c5799
AyeshaFarkhundah <22863396+AyeshaFarkhundah@users.noreply.github.com>
2019-04-06 23:51:50 -0400

    Add files via upload

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'here'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+?>
